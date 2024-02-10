a6968fd9a8cbeceb3e7caebe44dbcf9b2829b361
Nassim LOUNADI <nassimlnd37@gmail.com>
2024-02-10 18:47:43 +0100

    ✨ add laravel/sail for docker deployment

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -9,21 +9,6 @@ class FileController extends Controller
 {
     public function downloadAndStore()
     {
-        $url = "http://r365mail.biz:2103/movie/Cr5ZQUazyj/430866556543/38240.mkv";
 
-        $fileName = "test";
-
-        $destinationPath = public_path('movies/mkv/' . $fileName);
-
-        $command = "wget -O $url -P $destinationPath";
-        error_log("Command : " . $command);
-
-        exec($command, $output, $return);
-
-        if ($return === 0 && file_exists($destinationPath)) {
-            error_log("File downloaded successfully!");
-        } else {
-            error_log("File download failed!");
-        }
     }
 }
