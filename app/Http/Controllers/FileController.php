37a3bd1e4e299b4f15f6f112b56cd9ccff86207a
Nassim LOUNADI <nassimlnd37@gmail.com>
2024-02-11 13:20:47 +0100

    ✨ filecontroller update

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -14,20 +14,23 @@ class FileController extends Controller
         // Get the media from the request
         $movie = $request->post('media');
 
-        $command = 'wget -c ' . $movie['url'] . " ";
-
-        $status = 0;
-
-        for ($i = 0; $i < 5; $i++) {
-            exec($command, $output, $status);
-            if ($status === 0) {
-                break;
-            }
-        }
-
         $urlExploded = explode('/', $movie['url']);
-
         $fileName = end($urlExploded);
+
+        if (!file_exists($fileName)) {
+            $command = 'wget -c ' . $movie['url'] . " ";
+
+            $status = 0;
+
+            for ($i = 0; $i < 5; $i++) {
+                exec($command, $output, $status);
+                if ($status === 0) {
+                    break;
+                }
+            }
+        } elseif (file_exists($fileName . '.mp4')) {
+            return response()->json(['message' => 'File already exists']);
+        }
 
         $res = FileController::convertMkvToMp4($fileName);
 
